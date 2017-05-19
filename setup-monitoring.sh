4c70f8062c4b948e8d85fbf1b0d88de71c9fa85b
gilleslabs <gilles.labs.tosi@gmail.com>
2017-05-19 18:57:16 +0200

    added zabbix

--- setup-monitoring.sh
+++ setup-monitoring.sh
@@ -14,7 +14,7 @@ docker run \
 docker run \
    -d \
    --name zabbix \
-   -p 80:80 \
+   -p 81:80 \
    -p 10051:10051 \
    -v /etc/localtime:/etc/localtime:ro \
    --volumes-from zabbix-ext-all-templates \
