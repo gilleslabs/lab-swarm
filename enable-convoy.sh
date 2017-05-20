7a47ad7762ba898044d6ffe41f164934a3aaa831
gilleslabs <gilles.labs.tosi@gmail.com>
2017-05-20 18:43:05 +0200

    Stuff to make shares available even after VM reboot

--- enable-convoy.sh
+++ enable-convoy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sudo ufw disable
 #### Enabling Convoy storage driver plugin
 wget https://github.com/rancher/convoy/releases/download/v0.5.0/convoy.tar.gz
 tar xvf convoy.tar.gz
@@ -8,13 +9,19 @@ sudo mkdir -p /etc/docker/plugins/
 sudo bash -c 'echo "unix:///var/run/convoy/convoy.sock" > /etc/docker/plugins/convoy.spec'
 sudo service docker restart
 sudo mkdir /convoy-nfs
-sudo mkdir /shared-nfs
+sudo mkdir /nfs-share
+sudo mkdir /demo
 
-sudo mount -t nfs -o nolock 10.100.192.100:/convoy-share /convoy-nfs
-sudo mount -t nfs -o nolock 10.100.192.100:/nfs-share /shared-nfs
+sudo mount -t nfs -o nolock 10.100.192.100:/convoy-nfs /convoy-nfs
+sudo mount -t nfs -o nolock 10.100.192.100:/nfs-share /nfs-share
+sudo mount -t nfs -o nolock 10.100.192.100:/demo /demo
 
 sudo cp /vagrant/convoy /etc/init/convoy
 sudo ln -s /etc/init/convoy /etc/init.d/convoy
 sudo chmod 755 /etc/init.d/convoy
 sudo service convoy start
 
+
+sudo echo 10.100.192.100:/convoy-nfs /convoy-nfs nfs auto,nolock > /etc/fstab
+sudo echo 10.100.192.100:/nfs-share /nfs-share nfs auto,nolock >> /etc/fstab
+sudo echo 10.100.192.100:/demo /demo nfs auto,nolock >> /etc/fstab
--- install-nfs.sh
+++ install-nfs.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 
-#### Tempoary weird stuff ####
-
-sudo ufw disable
-
 #### Enabling Convoy storage driver plugin migth not be needed to be checked
 wget https://github.com/rancher/convoy/releases/download/v0.5.0/convoy.tar.gz
 tar xvf convoy.tar.gz
@@ -15,12 +11,18 @@ sudo bash -c 'echo "unix:///var/run/convoy/convoy.sock" > /etc/docker/plugins/co
 
 sudo apt-get update -y
 sudo apt-get install nfs-kernel-server -y
-sudo mkdir /convoy-share
-sudo echo /convoy-share *\(rw,sync,no_root_squash\) > /etc/exports
+
+sudo mkdir /convoy-nfs
+sudo echo /convoy-nfs *\(rw,sync,no_root_squash\) > /etc/exports
 
 sudo mkdir /nfs-share
 sudo echo /nfs-share *\(rw,sync,no_root_squash\) >> /etc/exports
+
+sudo mkdir /demo
+sudo cp -R /vagrant/demo/. /demo/.
+sudo chown -R vagrant:vagrant /demo
+sudo echo /demo *\(rw,sync,no_root_squash\) >> /etc/exports
+
 sudo service nfs-kernel-server start
 sudo exportfs -u
-
 
